816260c90b6a66363183a2c980773446c9f5e373
ZheMoon <1084878100@qq.com>
2017-08-30 22:53:36 +0800

    add helloworld

---
+++ helloworld/qiandao/Dockerfile
@@ -0,0 +1,5 @@
+FROM eboraas/apache-php:latest
+
+COPY src/ /var/www/html/
+
+RUN rm /var/www/html/index.html
---
+++ helloworld/qiandao/src/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Welcome to CTFhub!!!</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h3 align="center">欢迎来到CTFhub</h3>
+<p>CTFhub是面向所有学习CTF的朋友的环境，不用了解docker原理及知识，仅仅简单执行几条命令即可完成整个平台的搭建。<!-- 听说注释里面东西外面看不到？flag{123456 --></p>
+<br/>
+
+</body>
+</html>
+<?php 
+	header('flag: 789}');
+ ?>
